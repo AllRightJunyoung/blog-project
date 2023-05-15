ce902142d8e860f467c01ed8e59f2e3e42b4e705
TheGiantKiller <ktsdsd94@gmail.com>
2023-05-15 17:10:18 +0900

    chore : Next.js 폴더구조 잡기

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const HomePage = () => {
+  return (
+    <>
+      <Head>
+        <title>준영 블로그</title>
+        <meta name="description" content="I post about programming and web development." />
+      </Head>
+      <div>Main Page</div>
+    </>
+  );
+};
+
+export default HomePage;
