9e2b96f958b93425ab7c111804acc5a793ed8bf7
TheGiantKiller <ktsdsd94@gmail.com>
2023-05-16 16:01:25 +0900

    chore : ChakraUI 환경설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
+import { ChakraProviders } from "@/providers/chakraUI";
+import type { AppProps } from "next/app";
 import Head from "next/head";
-
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ChakraProviders>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
-    </>
+    </ChakraProviders>
   );
 }
 
