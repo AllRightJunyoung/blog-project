c76917078bc5c32e352b444f7e0a91316fa91f41
TheGiantKiller <ktsdsd94@gmail.com>
2023-05-30 20:48:41 +0900

    chore : mdx 블로그 환경설정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
+
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {
+    // If you use remark-gfm, you'll need to use next.config.mjs
+    // as the package is ESM only
+    // https://github.com/remarkjs/remark-gfm#install
+    remarkPlugins: [],
+    rehypePlugins: [],
+    // If you use `MDXProvider`, uncomment the following line.
+    // providerImportSource: "@mdx-js/react",
+  },
+});
+
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
@@ -34,6 +48,8 @@ const nextConfig = {
     domains: ["picsum.photos", "loremflickr.com"],
   },
   // ...other config
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
 };
 
-module.exports = nextConfig;
+// module.exports = nextConfig;
+module.exports = withMDX(nextConfig);
