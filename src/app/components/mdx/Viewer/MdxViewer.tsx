69e66524aacaee1e3b30c9c25e146273d20bbfc8
TheGiantKiller <ktsdsd94@gmail.com>
2023-06-01 15:32:34 +0900

    remove : 불필요한 컴포넌트 제거

--- src/app/components/mdx/Viewer/MdxViewer.tsx
+++ src/app/components/mdx/Viewer/MdxViewer.tsx
@@ -1,27 +1,17 @@
 import { MDXProvider } from "@mdx-js/react";
 import type { MDXProps } from "./MdxViewer.types";
 import { MDXComponents } from "mdx/types";
-import ResponsiveImage from "../Image/Image";
-import { LI, OL, UL } from "../LIST/LIST";
-import { H1, H2, H3, H4, H5, H6 } from "../Heading/Heading";
+import { ResponsiveImage, CodeBlock } from "@/app/components/mdx";
 
 const components = {
   img: ResponsiveImage,
-  h1: H1,
-  h2: H2,
-  h3: H3,
-  h4: H4,
-  h5: H5,
-  h6: H6,
-  li: LI,
-  ol: OL,
-  ul: UL,
+  code: CodeBlock,
 };
 
 const MdxViewer = ({ children }: MDXProps) => {
   return (
     <>
-      <MDXProvider>
+      <MDXProvider components={components as MDXComponents}>
         <div className="markdown-body">{children}</div>
       </MDXProvider>
     </>
