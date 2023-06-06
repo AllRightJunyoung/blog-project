4488787c044f5d4e0a56c888d0326fe2ee116485
TheGiantKiller <ktsdsd94@gmail.com>
2023-06-06 18:39:29 +0900

    feat : Markdown Code Syntax highlighter 추가

--- src/app/components/markdown/Markdown.tsx
+++ src/app/components/markdown/Markdown.tsx
@@ -5,12 +5,29 @@ import { Box } from "@chakra-ui/react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import Image from "next/image";
 import remarkGfm from "remark-gfm";
+
 import type { MarkdownProps } from "./Markdown.types";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 const customMarkDownComponent = {
   img: (image: any) => {
     return (
       <Image src={`${image.src}`} alt={image.alt} width={600} height={400} />
+    );
+  },
+  code({ node, inline, className, children, ...props }) {
+    const match = /language-(\w+)/.exec(className || "");
+    return !inline && match ? (
+      <SyntaxHighlighter
+        language={match[1]}
+        PreTag="div"
+        {...props}
+        style={atomDark}
+      >
+        {String(children).replace(/\n$/, "")}
+      </SyntaxHighlighter>
+    ) : (
+      <code {...props}>{children}</code>
     );
   },
 };
