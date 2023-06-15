c83c15622d8e1857c8754824bcda73993b75c8ee
TheGiantKiller <ktsdsd94@gmail.com>
2023-06-15 09:33:04 +0900

    feat : FeaturedPost 컴포넌트 ReadPost 추가

--- src/app/components/home/FeaturedPost/Carousel/Carousel.tsx
+++ src/app/components/home/FeaturedPost/Carousel/Carousel.tsx
@@ -8,7 +8,7 @@ import uuid from "react-uuid";
 import { Text, Image, Flex, Box } from "@chakra-ui/react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Autoplay } from "swiper";
-import { BellIcon, SunIcon, CalendarIcon } from "@chakra-ui/icons";
+import { BellIcon, SunIcon, CalendarIcon, LinkIcon } from "@chakra-ui/icons";
 import { FeaturedPostTags } from "../FeaturedPostTags";
 import { useRouter } from "next/navigation";
 
@@ -29,37 +29,32 @@ const FeautredPostCarousel = ({ data }: CarouselProps) => {
               <Flex direction="column" gap={2}>
                 <FeaturedPostTags tags={data[idx].tags} />
                 <Flex gap={1} direction="column" marginBottom={5}>
-                  <Flex
-                    alignItems="center"
-                    gap={2}
-                    cursor="pointer"
-                    onClick={() => router.push(`/posts/${post.slug}`)}
-                  >
+                  <Flex alignItems="center" gap={2}>
                     <BellIcon color="yellow" w={6} h={6} />
                     <Text fontSize="3xl" color="white">
                       {post.title}
                     </Text>
                   </Flex>
-                  <Flex
-                    alignItems="center"
-                    gap={2}
-                    cursor="pointer"
-                    onClick={() => router.push(`/posts/${post.slug}`)}
-                  >
+                  <Flex alignItems="center" gap={2} marginBottom={1}>
                     <SunIcon color="orange" w={5} h={5} />
                     <Text fontSize="2xl" color="white">
                       {post.description}
                     </Text>
                   </Flex>
-                  <Flex
-                    alignItems="center"
-                    gap={3}
-                    cursor="pointer"
-                    onClick={() => router.push(`/posts/${post.slug}`)}
-                  >
+                  <Flex alignItems="center" gap={3} marginBottom={7}>
                     <CalendarIcon color="white" w={4} h={4} />
                     <Text fontSize="lg" color="white">
                       {post.date}
+                    </Text>
+                  </Flex>
+                  <Flex alignItems="center" gap={3} cursor="pointer">
+                    <LinkIcon color="white" w={4} h={4} />
+                    <Text
+                      fontSize="2xl"
+                      color="white"
+                      onClick={() => router.push(`/posts/${post.slug}`)}
+                    >
+                      {"READ POST"}
                     </Text>
                   </Flex>
                 </Flex>
