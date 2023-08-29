70147c77ef3f7d6bd648845d1e9043a9ab7faa47
TheGiantKiller <ktsdsd94@gmail.com>
2023-08-29 16:28:27 +0900

    feat : PostDetail Page fadeIn 애니메이션 적용

--- src/app/components/template/PostDetailSection/PostDetailSection.tsx
+++ src/app/components/template/PostDetailSection/PostDetailSection.tsx
@@ -2,7 +2,6 @@
 
 import ListIcon from "public/icons/toc/list.svg";
 import CloseIcon from "public/icons/toc/close.svg";
-
 import { HorizontalHeading } from "../../common";
 import { Box, Text, Flex, useMediaQuery } from "@chakra-ui/react";
 import { TimeIcon, InfoOutlineIcon } from "@chakra-ui/icons";
@@ -12,6 +11,8 @@ import { useRecoilState } from "recoil";
 import { TocModalState } from "../../post/recoil";
 import MarkdownViewer from "../../markdown/MarkdownViewer";
 import { TableOfContents } from "../../post/Toc";
+import { motion } from "framer-motion";
+import { fadeIn } from "@/app/styles/framer-motions";
 
 const PostDetailSection = ({
   title,
@@ -27,51 +28,53 @@ const PostDetailSection = ({
   const [activeTocModal, setActiveTocModal] = useRecoilState(TocModalState);
 
   return (
-    <article>
-      {isSmallerThan960 && (
-        <Box
-          position="fixed"
-          right="0.75rem"
-          bottom="1.25rem"
-          cursor="pointer"
-          zIndex={30}
-          onClick={() => setActiveTocModal(!activeTocModal)}
-        >
-          {!activeTocModal ? (
-            <ListIcon width="60" height="60" />
-          ) : (
-            <CloseIcon width="80" height="80" />
-          )}
-        </Box>
-      )}
-      <Box>
-        <Box padding={10}>
-          <HorizontalHeading title={title} fontSize="4xl" />
-          <Flex gap={5}>
-            <Flex alignItems="center" gap={2} marginTop={3} marginBottom={3}>
-              <InfoOutlineIcon color="white" />
-              <Text color="white" fontSize="md">
-                {category}
-              </Text>
-            </Flex>
-
-            <Flex alignItems="center" gap={1} marginTop={3} marginBottom={3}>
-              <TimeIcon color="white" />
-              <Text color="white" fontSize="md">
-                {date}
-              </Text>
-            </Flex>
-          </Flex>
-          <Box marginTop={5}>
-            <PostDetailTags tags={tags} />
+    <motion.div variants={fadeIn} initial="hidden" animate="visible">
+      <article>
+        {isSmallerThan960 && (
+          <Box
+            position="fixed"
+            right="0.75rem"
+            bottom="1.25rem"
+            cursor="pointer"
+            zIndex={30}
+            onClick={() => setActiveTocModal(!activeTocModal)}
+          >
+            {!activeTocModal ? (
+              <ListIcon width="60" height="60" />
+            ) : (
+              <CloseIcon width="80" height="80" />
+            )}
           </Box>
+        )}
+        <Box>
+          <Box padding={10}>
+            <HorizontalHeading title={title} fontSize="4xl" />
+            <Flex gap={5}>
+              <Flex alignItems="center" gap={2} marginTop={3} marginBottom={3}>
+                <InfoOutlineIcon color="white" />
+                <Text color="white" fontSize="md">
+                  {category}
+                </Text>
+              </Flex>
+
+              <Flex alignItems="center" gap={1} marginTop={3} marginBottom={3}>
+                <TimeIcon color="white" />
+                <Text color="white" fontSize="md">
+                  {date}
+                </Text>
+              </Flex>
+            </Flex>
+            <Box marginTop={5}>
+              <PostDetailTags tags={tags} />
+            </Box>
+          </Box>
+          <Flex padding={3} gap={5}>
+            <MarkdownViewer>{content}</MarkdownViewer>
+            <TableOfContents isSmallerThan960={isSmallerThan960} />
+          </Flex>
         </Box>
-        <Flex padding={3} gap={5}>
-          <MarkdownViewer>{content}</MarkdownViewer>
-          <TableOfContents isSmallerThan960={isSmallerThan960} />
-        </Flex>
-      </Box>
-    </article>
+      </article>
+    </motion.div>
   );
 };
 
