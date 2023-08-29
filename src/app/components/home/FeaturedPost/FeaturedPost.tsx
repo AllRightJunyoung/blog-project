71796b0d8c0aaa3316381924797d5e874b2d2a97
TheGiantKiller <ktsdsd94@gmail.com>
2023-08-29 17:41:54 +0900

    feat : FeaturedPost 컴포넌트 fadeInLeft 애니메이션 적용

--- src/app/components/home/FeaturedPost/FeaturedPost.tsx
+++ src/app/components/home/FeaturedPost/FeaturedPost.tsx
@@ -1,24 +1,27 @@
 "use client";
-
+import { motion } from "framer-motion";
 import { Flex, Heading, Box } from "@chakra-ui/react";
 import { useRecoilValue } from "recoil";
 import { FeaturedPostsState } from "../recoil";
 import FeautredPostCarousel from "./Carousel";
+import { fadeInFromLeft } from "@/app/styles/framer-motions";
 
 const FeaturedPost = () => {
   const featuredPost = useRecoilValue(FeaturedPostsState);
 
   return (
-    <Flex direction="column" alignItems="center">
-      <Box>
-        <Flex direction="column" gap={5}>
-          <Heading as="h1" color="white" fontStyle="italic" padding={5}>
-            Featured Post
-          </Heading>
-          <FeautredPostCarousel data={featuredPost} />
-        </Flex>
-      </Box>
-    </Flex>
+    <motion.div variants={fadeInFromLeft} initial="hidden" animate="visible">
+      <Flex direction="column" alignItems="center">
+        <Box>
+          <Flex direction="column" gap={5}>
+            <Heading as="h1" color="white" fontStyle="italic" padding={5}>
+              Featured Post
+            </Heading>
+            <FeautredPostCarousel data={featuredPost} />
+          </Flex>
+        </Box>
+      </Flex>
+    </motion.div>
   );
 };
 
