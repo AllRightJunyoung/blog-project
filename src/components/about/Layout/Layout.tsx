a9aa36fdb6da19484f94badd3a600703c52f9499
TheGiantKiller <ktsdsd94@gmail.com>
2023-12-10 07:45:22 +0900

    feat : 자기소개 페이지 내용 수정

--- src/components/about/Layout/Layout.tsx
+++ src/components/about/Layout/Layout.tsx
@@ -39,12 +39,28 @@ const AboutLayout = () => {
               query={["문준영"]}
               styles={{ px: "2", py: "1", rounded: "full", bg: "teal.400" }}
             >
-              안녕하세요 프론트엔드 개발자 문준영 입니다.
+              안녕하세요 프론트엔드 개발자를 꿈꾸는 문준영 입니다.
             </Highlight>
           </Text>
           <Text fontSize="18px" color="white" fontWeight="bold">
             상상한것을 코드로 구현했을때 즐거움을 느끼고 다른사람들에게 도움이
             되는 서비스를 만드는것을 지향합니다.
+          </Text>
+          <Text fontSize="18px" color="white" fontWeight="bold">
+            <Highlight
+              query={["함께"]}
+              styles={{ px: "2", py: "1", rounded: "full", bg: "teal.400" }}
+            >
+              사람들과 커뮤니케이션 하며 함께 성장하는것을 좋아합니다.
+            </Highlight>
+          </Text>
+          <Text fontSize="18px" color="white" fontWeight="bold">
+            <Highlight
+              query={["헬스케어"]}
+              styles={{ px: "2", py: "1", rounded: "full", bg: "teal.400" }}
+            >
+              도메인은 헬스케어 부분을 선호하고 있습니다.
+            </Highlight>
           </Text>
 
           <Text fontSize="18px" color="white" fontWeight="bold">
@@ -57,11 +73,20 @@ const AboutLayout = () => {
           </Text>
           <Text fontSize="18px" color="white" fontWeight="bold">
             <Highlight
+              query={["웨이트", "8년", "삼대중량", "500"]}
+              styles={{ px: "2", py: "1", rounded: "full", bg: "teal.400" }}
+            >
+              취미는 아침마다 웨이트를 8년째 하고 있고 삼대중량 500이상을 치며
+              보디빌딩 쇼를 보는것을 좋아합니다.
+            </Highlight>
+          </Text>
+          <Text fontSize="18px" color="white" fontWeight="bold"></Text>
+          <Text fontSize="18px" color="white" fontWeight="bold">
+            <Highlight
               query={["웨이트", "등산", "8년"]}
               styles={{ px: "2", py: "1", rounded: "full", bg: "teal.400" }}
             >
-              취미는 웨이트를 8년째 하고 있고 보디빌딩쇼를 보는것을 좋아합니다.
-              그리고 주말에 종종 등산가는것을 좋아합니다.
+              주말에 종종 등산을 하며 주변 경치를 보며 힐링하러 갑니다.
             </Highlight>
           </Text>
 
@@ -75,10 +100,20 @@ const AboutLayout = () => {
           </Text>
           <Text fontSize="18px" color="white" fontWeight="bold">
             <Highlight
+              query={["ReactNative", "TypeScript"]}
+              styles={{ px: "2", py: "1", rounded: "full", bg: "teal.400" }}
+            >
+              서버 상태관리라이브러리 웹에서 발생하는 성능 문제 해결 사용자
+              UI/UX 개선 ReactNative에 관심이있습니다
+            </Highlight>
+          </Text>
+          <Text fontSize="18px" color="white" fontWeight="bold">
+            <Highlight
               query={["애자일"]}
               styles={{ px: "2", py: "1", rounded: "full", bg: "teal.400" }}
             >
-              업무방식은 워터풀 방식보다 애자일 한것을 선호합니다.
+              업무방식은 워터풀 방식보다 빠르게 변화하는 애자일 한것을
+              선호합니다.
             </Highlight>
           </Text>
         </Flex>
